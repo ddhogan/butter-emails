8feecb3fa3fcd3a25cff21b903796bf5bacde737
Donna <donnamcla@gmail.com>
2018-09-17 10:45:52 -0400

    fixed postInput issue where post content wasn't appearing

--- client/src/components/PostInput.jsx
+++ client/src/components/PostInput.jsx
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class PostInput extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { content: '', };
+  }
+
+  handleChange(event) {
+    this.setState({ content: event.target.value });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.createPost(this.state);
+    this.setState({ content: '', });
+  }
+
   render () {
     return (
       <div>
-        <form>
-          <input type="text" />
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Create Post: </label>
+          <input 
+            type="text"
+            value={this.state.content}
+            onChange={(event) => this.handleChange(event)} 
+          />
           <input type="submit" />
         </form>
       </div>
@@ -13,4 +34,8 @@ class PostInput extends Component {
   }
 }
 
-export default PostInput;
+const mapDispatchToProps = dispatch => ({
+  createPost: formData => dispatch({ type: "CREATE_POST", payload: formData })
+})
+
+export default connect(null, mapDispatchToProps)(PostInput);
--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -1,11 +1,11 @@
-export default function postsReducer(state ={
+export default function postsReducer(state = {
   posts: [],
 }, action) {
   switch(action.type){
     case "FETCH_POSTS":
       return state.posts
     case "CREATE_POST":
-      return state.concat(action.payload)
+      return { posts: state.posts.concat(action.payload) }
     default:
       return state;
   };
