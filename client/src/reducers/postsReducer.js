d7b66fd09615c0bbb4d422722477df3408141ee4
Donna <donnamcla@gmail.com>
2018-09-16 21:07:45 -0400

    added some actions

--- client/src/reducers/postsReducer.js
+++ client/src/reducers/postsReducer.js
@@ -1,5 +1,12 @@
 export default function postsReducer(state ={
   posts: [],
 }, action) {
-  return state;
+  switch(action.type){
+    case "FETCH_POSTS":
+      return state.posts
+    case "CREATE_POST":
+      return state.concat(action.payload)
+    default:
+      return state;
+  };
 }
