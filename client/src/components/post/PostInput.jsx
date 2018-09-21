fa94e33194345d73fea614a5c847707afbae356b
Donna <donnamcla@gmail.com>
2018-09-21 12:03:10 -0400

    small styling changes

--- client/src/components/post/PostInput.jsx
+++ client/src/components/post/PostInput.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from '../../actions/postActions';
 import { bindActionCreators } from 'redux';
+import '../../css/PostInput.css';
 
 class PostInput extends Component {
   constructor() {
