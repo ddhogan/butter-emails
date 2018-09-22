b08c1df718bbb539575efe6cdd7f27a9465696fd
Donna <donnamcla@gmail.com>
2018-09-22 19:16:25 -0400

    convert About component to functional syntax

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class About extends Component {
-  render() {
-    return (
-      <div className="about">
-        <h2>About ButterEmails</h2>
-        <p>ButterEmails is another tiny Twitter clone.  Exept instead of Nazis, you can edit your posts!</p>
-        <p>This is also a React on Rails portfolio project for the Flatiron School's web development bootcamp.</p>
-      </div>
-    )
-  }
+const About = () => {
+  return (
+    <div className="about">
+      <h2>About ButterEmails</h2>
+      <p>ButterEmails is another tiny Twitter clone.  Exept instead of Nazis, you can edit your posts!</p>
+      <p>This is also a React on Rails portfolio project for the Flatiron School's web development bootcamp.</p>
+    </div>
+  )
 }
 
 export default About;
