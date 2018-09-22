f208b872a673d51c2b36cbcd7bc6218457cd3433
Donna <donnamcla@gmail.com>
2018-09-22 13:42:16 -0400

    added form to Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,13 +2,43 @@ import React, { Component } from 'react';
 import '../css/Login.css';
 
 class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      username: '',
+      password: ''
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // login
+  }
+  handleChange = (event) => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+  
   render() {
     return (
       <div className="login">
-        <h2>Login or Signup</h2>
-        <p>This will be where users log in or sign up.</p>
+        <h2>Login</h2>
+        <form onSubmit={(event) => this.handleSubmit(event)}>
+          <label>Username: </label>
+          <input 
+          type="text"
+          name="username"
+          value={this.state.username}
+          onChange={(event) => this.handleChange(event)}
+          />
+          <br/>
+          <label>Password: </label>
+          <input type="password" name="password"/>
+          <br/>
+          <button type="submit">Log In</button>
+        </form>
         <ul>
-          <li>email</li>
+          <li>Need to add a signup route and Component</li>
           <li>OAuth (maybe twitter?)</li>
           <li>Maybe <a href="https://openid.net/connect/">OpenID</a>, because OAuth is <a href="https://oauth.net/articles/authentication/">annoying.</a></li>
         </ul>
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-post = Post.create({ content: 'this is the very first post!' })
-
 borpo = User.create(username: 'borpo', password_digest: BCrypt::Password.create('password'))
 borpo.save
-
 Post.create(content: 'borpo will destroy you', user: borpo)
+Post.create(content: 'borpo just had a mimosa', user: borpo)
+
+ladyShellington = User.create(username: 'LadyShellington', password_digest: BCrypt::Password.create('chicken'))
+ladyShellington.save
+Post.create(content: 'i am a chicken', user: ladyShellington)
+Post.create(content: 'In preparing for battle I have always found that plans are useless, but planning is indispensable.', user: ladyShellington)
