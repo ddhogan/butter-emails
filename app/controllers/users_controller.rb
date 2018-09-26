1a960eaff3cbed55c2687b8f799c7591d67697a0
Donna <donnamcla@gmail.com>
2018-09-26 17:20:32 -0400

    removed obsolete methods from user controler, edited the history push in the Login component

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,34 +16,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = @user.id
       render json: @user
     else
       render json: { message: @user.errors }, status: :unprocessable_entity
-    end
-  end
-
-  def login
-    @user = User.find_by(username: params[:user][:username])
-    if @user && @user.authenticate(params[:user][:password])
-      session[:user_id] = @user.id
-      render json: @user
-    else
-      redirect_to login_path
-    end
-  end
-
-  def logout
-    if session[:user_id]
-      session.clear
-      redirect_to root_path
     end
   end
   
   private
 
   def user_params
-    params.require(:user).permit(:username, :password, :password_confirmation, :post_ids => [])
+    params.require(:user).permit(:username, :email, :password, :password_confirmation, :post_ids => [])
   end
 
 end
