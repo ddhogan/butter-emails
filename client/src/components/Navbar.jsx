2fdd4afe172f391c96094f90dae2d5d2e7ea27b8
Donna <donnamcla@gmail.com>
2018-09-27 20:11:27 -0400

    i don't even know anymore

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import { logout } from '../actions/authActions';
 
 class Navbar extends Component {
-  constructor(props) {
-    super(props)
-  }
+  // constructor(props) {
+  //   super(props)
+  // }
 
   handleLogout = (e) => {
     e.preventDefault();
