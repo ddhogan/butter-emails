4162f590e68a9056d0b119b4b54b6cd2ffccdc99
Donna <donnamcla@gmail.com>
2018-09-28 12:19:43 -0400

    removed some already commented out code

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from 'redux';
 import postsReducer from './postsReducer';
-// import usersReducer from './usersReducer';
 import authReducer from './authReducer';
 
 const rootReducer = combineReducers({
   posts: postsReducer,
-  // users: usersReducer,
   auth: authReducer,
 })
 
