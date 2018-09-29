146e5d9e283e0204b916117c2576801ccf30b8fc
Donna <donnamcla@gmail.com>
2018-09-29 11:02:52 -0400

    adjusted ternary if statement so you have to be logged in and the post.user_id has to match yours in order to see the edit/delete buttons

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -17,12 +17,11 @@ class Post extends Component {
       <div className="post">
         <small>{this.props.post.user.username} writes: </small>
         <p>{this.props.post.content}  
-        { this.props.auth.isAuthenticated ? 
+        { this.props.auth.currentUser.id === this.props.post.user_id ? // you have to be logged in and they have to be your posts to see these buttons
           <React.Fragment>
             <button onClick={this.handleDeleteOnClick}>X </button>
             <button onClick={this.handleEditOnClick}>Edit </button>
-          </React.Fragment>
-        : null}
+          </React.Fragment> : null}
         </p>
         <small>{dateFormat(this.props.post.created_at)}, Post ID {this.props.post.id}</small>
       </div>
@@ -33,3 +32,5 @@ class Post extends Component {
 const mapStateToProps = state => ({ auth: state.auth });
 
 export default connect(mapStateToProps, null)(Post);
+
+// this.props.auth.isAuthenticated
