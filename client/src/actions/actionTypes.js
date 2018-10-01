11439128a2d1eb5c632c28215394afbea193ab22
Donna <donnamcla@gmail.com>
2018-09-30 20:53:38 -0400

    reformatted

--- client/src/actions/actionTypes.js
+++ client/src/actions/actionTypes.js
@@ -1,10 +1,9 @@
 
 // Post Actions
-export const CREATE_POST = "CREATE_POST"
-export const DELETE_POST = "DELETE_POST"
 export const LOADING_POSTS = "LOADING_POSTS"
 export const FETCH_POSTS = "FETCH_POSTS"
-export const EDIT_POST = "EDIT_POST"
+export const CREATE_POST = "CREATE_POST"
+export const DELETE_POST = "DELETE_POST"
 export const UPDATE_POST = "UPDATE_POST"
 
 // Auth Action
